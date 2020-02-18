e9f448f013d21ac4d146ab421b1af8e54974c921
bacancy <khushbu.kosti@bacancytechnology.com>
2020-02-18 18:24:11 +0530

    commit4

--- src/info.js
+++ src/info.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
 import students from './student_data.json';
-import { BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import {  Switch, Route, Link} from 'react-router-dom';
 import Detail from './detail';
 import Marks from './marks'
-import NoMatch from './NoMatch'
 
 
 class Info extends React.Component{
-
     render(){
-        const {params}=this.props.match;        
+        const {params,path}=this.props.match;  
+        console.log(this.props.match)
         let data=students.find(student=> student.id == params.id )
         return(<div>
                 <h5>Id- {data.id}</h5>
@@ -23,9 +22,8 @@ class Info extends React.Component{
                     <Button variant="dark">Marks</Button>
                 </Link>
                 <Switch>
-                    <Route path={`/student/:id/info`} component={Detail} /> 
-                    <Route path={`/student/:id/marks`} component={Marks} /> 
-                    
+                    <Route path={`${path}/info`} component={Detail} /> 
+                    <Route path={`${path}/marks`} component={Marks} /> 
                </Switch>
             </div>
         )
--- src/studentlist.js
+++ src/studentlist.js
@@ -1,54 +1,53 @@
 import React from 'react';
-import {Table} from 'react-bootstrap';
+import { Table, Card } from 'react-bootstrap';
 import data from './student_data.json';
-import {Link} from 'react-router-dom' 
+import { Link } from 'react-router-dom'
 import './App.css';
 import NoMatch from './NoMatch'
 
 
- class StudentList extends React.Component{
-     state={
-         students:[]
-     }
-     componentDidMount() {
-         this.setState({
-             students:data
-         })
-     }
-     render(){
-         return(
-             <div>
-                <Table striped bordered hover>
-                <thead>
-                    <tr>
-                    <th>Id</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.students.map( student=>
-                        {
-                            return <tr>
-                                <td>{student.id}</td>
-                                <td>{student.first_name}</td>
-                                <td>{student.last_name}</td>
-                                <td>
-                                    <Link to={`/student/${student.id}`}>
-                                        <input type="button" 
-                                            value="Detail">
-                                        </input>
-                                    </Link>
-                                </td>
+class StudentList extends React.Component {
+    state = {
+        students: []
+    }
+    componentDidMount() {
+        this.setState({
+            students: data
+        })
+    }
+    render() {
+        return (
+            <div>
+                    <Table striped bordered hover size="sm" responsive>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Action</th>
                             </tr>
-                        }
-                        )}
-                </tbody>
-            </Table>
+                        </thead>
+                        <tbody>
+                            {this.state.students.map(student => {
+                                return <tr>
+                                    <td>{student.id}</td>
+                                    <td>{student.first_name}</td>
+                                    <td>{student.last_name}</td>
+                                    <td>
+                                        <Link to={`/student/${student.id}`}>
+                                            <input type="button"
+                                                value="Detail">
+                                            </input>
+                                        </Link>
+                                    </td>
+                                </tr>
+                            }
+                            )}
+                        </tbody>
+                    </Table>
             </div>)
-         
-         }
- }
+
+    }
+}
 
 export default StudentList;
